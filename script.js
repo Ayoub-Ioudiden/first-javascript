ba47c754c37c805e8de475c63030bb06afaf519a
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2021-11-24 13:20:13 +0100

    variables(let) console.log

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("hello winc academy")
+
+let firstName = 'Ayoub';
+console.log(firstName);
+
+//not possible, 'if' is a reserved keyword
+
+// number literal possible, it solves the equation
+let solution = 4 + 4;
+console.log(solution);
+
+//possible but it prints the two separate numbers next to eachother making it "44"
+let name = "4" + "4";
+console.log(name);
+
+let optellen = 12 + 12;
+console.log(optellen);
+
+let aftrekken = 25 - 13;
+console.log(aftrekken);
+
+let vermenigvuldigen = 5 * 7;
+console.log(vermenigvuldigen);
+
+let delen = 20 / 5;
+console.log(vermenigvuldigen);
+
+//typeof age was first number then turned into string
+let age = '25';
+console.log(age);
+
+
+
+
